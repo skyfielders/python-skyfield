f5e07b355984b0a86a890ee0b6977ec8818be248
Brandon Rhodes <brandon@rhodesmill.org>
2013-08-07 01:04:33 -0400

    Clean up how framelib matrices are constructed

--- skyfield/framelib.py
+++ skyfield/framelib.py
@@ -4,34 +4,31 @@ from numpy import array
 
 ASEC2RAD = 4.848136811095359935899141e-6
 
-# 'xi0', 'eta0', and 'da0' are ICRS frame biases in arcseconds taken
-# from IERS (2003) Conventions, Chapter 5.
+def build_matrix():
+    # 'xi0', 'eta0', and 'da0' are ICRS frame biases in arcseconds taken
+    # from IERS (2003) Conventions, Chapter 5.
 
-xi0  = -0.0166170
-eta0 = -0.0068192
-da0  = -0.01460
+    xi0  = -0.0166170 * ASEC2RAD
+    eta0 = -0.0068192 * ASEC2RAD
+    da0  = -0.01460   * ASEC2RAD
 
-# Compute elements of rotation matrix to first order the first time this
-# function is called.  Elements will be saved for future use and not
-# recomputed.
+    # Compute elements of rotation matrix.
 
-xx =  1.0
-yx = -da0  * ASEC2RAD
-zx =  xi0  * ASEC2RAD
-xy =  da0  * ASEC2RAD
-yy =  1.0
-zy =  eta0 * ASEC2RAD
-xz = -xi0  * ASEC2RAD
-yz = -eta0 * ASEC2RAD
-zz =  1.0
+    yx = -da0
+    zx =  xi0
+    xy =  da0
+    zy =  eta0
+    xz = -xi0
+    yz = -eta0
 
-# Include second-order corrections to diagonal elements.
+    # Include second-order corrections to diagonal elements.
 
-xx = 1.0 - 0.5 * (yx * yx + zx * zx)
-yy = 1.0 - 0.5 * (yx * yx + zy * zy)
-zz = 1.0 - 0.5 * (zy * zy + zx * zx)
+    xx = 1.0 - 0.5 * (yx * yx + zx * zx)
+    yy = 1.0 - 0.5 * (yx * yx + zy * zy)
+    zz = 1.0 - 0.5 * (zy * zy + zx * zx)
 
-# The actual matrices.
+    return array(((xx, xy, xz), (yx, yy, yz), (zx, zy, zz)))
 
-J2000_to_ICRS = array(((xx, xy, xz), (yx, yy, yz), (zx, zy, zz)))
-ICRS_to_J2000 = array(((xx, yx, zx), (xy, yy, zy), (xz, yz, zz)))
+
+J2000_to_ICRS = build_matrix()
+ICRS_to_J2000 = J2000_to_ICRS.T
