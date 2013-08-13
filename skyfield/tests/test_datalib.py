71cc88836fde487a0e3500b262cccdb101cac3c8
Alex Tucker <tucker.al@gmail.com>
2013-08-13 14:29:43 -0400

    Cleaned up

--- skyfield/tests/test_datalib.py
+++ skyfield/tests/test_datalib.py
@@ -1,6 +1,5 @@
 from skyfield.datalib import download_file, is_days_old
 from datetime import datetime, timedelta
-import pytest
 import httpretty
 import os
 
@@ -24,25 +23,6 @@ def test_simple_download_days_old_0():
     assert open('data.txt', 'r').read() == 'FOOBAR'
     os.remove('data.txt')
 
-def test_is_days_old_true():
-    write_file('data.txt', 'BAZ')
-    d = datetime.today()-timedelta(hours=6)
-    unix_ago = int(d.strftime('%s'))
-    os.utime('data.txt', (unix_ago, unix_ago))
-
-    assert is_days_old('data.txt', 1) == False
-
-    os.remove('data.txt')
-
-def test_is_days_old_false():
-    write_file('data.txt', 'BAZ')
-    d = datetime.today()-timedelta(hours=48)
-    unix_ago = int(d.strftime('%s'))
-    os.utime('data.txt', (unix_ago, unix_ago))
-
-    assert is_days_old('data.txt', 1) == True
-    os.remove('data.txt')
-
 @httpretty.activate
 def test_simple_download_days_old_1():
     httpretty.register_uri(httpretty.GET, 'http://foo.com/data.txt',
@@ -54,13 +34,25 @@ def test_simple_download_days_old_1():
     assert open('data.txt', 'r').read() == 'BAZ'
     os.remove('data.txt')
 
-def write_file(filename, data, hours_ago=False):
+def test_is_days_old_true():
+    write_file('data.txt', 'BAZ')
+    d = datetime.today()-timedelta(hours=6)
+    unix_ago = int(d.strftime('%s'))
+    os.utime('data.txt', (unix_ago, unix_ago))
+
+    assert is_days_old('data.txt', 1) == False
+    os.remove('data.txt')
+
+def test_is_days_old_false():
+    write_file('data.txt', 'BAZ')
+    d = datetime.today()-timedelta(hours=48)
+    unix_ago = int(d.strftime('%s'))
+    os.utime('data.txt', (unix_ago, unix_ago))
+
+    assert is_days_old('data.txt', 1) == True
+    os.remove('data.txt')
+
+def write_file(filename, data):
     f = open(filename, 'w')
     f.write(data)
-
-    if hours_ago != False:
-        d = datetime.today()-timedelta(hours=hours_ago)
-        unix_ago = int(d.strftime('%s'))
-        os.utime(filename, (unix_ago, unix_ago))
-
 
