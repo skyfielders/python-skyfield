3c43466e3688cb8965a850cf7bb4ebfb363568d6
Alex Tucker <tucker.al@gmail.com>
2013-08-13 14:22:48 -0400

    days_old working properly

--- skyfield/datalib.py
+++ skyfield/datalib.py
@@ -2,13 +2,16 @@ import requests
 import os
 from datetime import datetime, timedelta
 
-class DownloadFile(object):
 
-    def __init__(self, url, filename, days_old=0):
-        response = requests.get(url, stream=True)
-        f = open(filename, 'w')
-        for chunk in response.iter_content(1024):
-            f.write(chunk)
+def download_file(url, filename, days_old=0):
+    if os.path.exists(filename):
+        if not is_days_old(filename, days_old):
+            return
+
+    response = requests.get(url, stream=True)
+    f = open(filename, 'w')
+    for chunk in response.iter_content(1024):
+        f.write(chunk)
 
 def is_days_old(filename, days_old):
     min_old = datetime.now()-timedelta(days=days_old)
