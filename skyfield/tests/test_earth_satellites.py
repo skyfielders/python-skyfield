6ae7faf15e269247ffaf848671d6ec789712af90
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-26 19:15:31 -0500

    Switch to heavens-above.com data for ISS test

--- skyfield/tests/test_earth_satellites.py
+++ skyfield/tests/test_earth_satellites.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pytest
 from datetime import datetime, timedelta
 from numpy import array
@@ -13,26 +15,29 @@ ISS (ZARYA)             \n\
 2 25544  51.6484  23.7537 0001246  74.1647  18.7420 15.50540527859894\n\
 """)
 
-spaceweather_transits = """\
-11/25/2013      ISS     05:42:39 am     NNW     05:45:50        40.2    -2.9
-11/26/2013      ISS     04:55:49 am     NNE     04:58:18        28.6    -2.2
-11/26/2013      ISS     06:29:22 am     WNW     06:32:36        48.1    -3.3
-11/27/2013      ISS     05:41:54 am     WNW     05:45:15        85.6    -4.0
-11/28/2013      ISS     04:55:07 am     ENE     04:58:25        52.9    -3.4
-11/29/2013      ISS     05:41:15 am     W       05:44:15        30.8    -2.3
-11/30/2013      ISS     04:54:32 am     SSE     04:57:48        34.2    -2.6
-"""
+heavens_above_transits = """\
+26 Nov	-1.7	04:55:55	29°	NNE	04:55:55	29°	NNE	04:58:45	10°	E	visible
+27 Nov	0.1	04:09:07	12°	ENE	04:09:07	12°	ENE	04:09:25	10°	ENE	visible
+27 Nov	-3.4	05:42:00	26°	WNW	05:43:45	86°	SW	05:47:05	10°	SE	visible
+28 Nov	-2.6	04:55:15	52°	ENE	04:55:15	52°	ENE	04:58:07	10°	ESE	visible
+29 Nov	0.1	04:08:35	13°	E	04:08:35	13°	E	04:08:58	10°	E	visible
+29 Nov	-2.2	05:41:28	25°	WSW	05:42:30	31°	SW	05:45:28	10°	SSE	visible
+30 Nov	-1.9	04:54:52	33°	SSE	04:54:52	33°	SSE	04:56:55	10°	SE	visible
+01 Dec	-0.9	05:41:13	12°	SW	05:41:13	12°	SW	05:42:15	10°	SSW	visible
+02 Dec	-0.4	04:54:46	10°	S	04:54:46	10°	S	04:54:49	10°	S	visible
+""".decode('utf-8')
 
-@pytest.fixture(params=spaceweather_transits.splitlines())
-def spaceweather_transit(request):
+@pytest.fixture(params=heavens_above_transits.splitlines())
+def iss_transit(request):
     line = request.param
     fields = line.split()
-    dt = datetime.strptime(fields[0] + ' ' + fields[5], '%m/%d/%Y %H:%M:%S')
-    altitude = float(fields[6])
+    dt = datetime.strptime('2013 {0} {1} {6}'.format(*fields),
+                           '%Y %d %b %H:%M:%S')
+    altitude = float(fields[7][:-1])
     return dt, altitude
 
-def test_iss_altitude(spaceweather_transit):
-    dt, altitude = spaceweather_transit
+def test_iss_altitude(iss_transit):
+    dt, altitude = iss_transit
     cst = timedelta(hours=-6) #, minutes=1)
     dt = dt - cst
     line0, line1, line2 = iss_tle.splitlines()
@@ -50,11 +55,4 @@ def test_iss_altitude(spaceweather_transit):
     diff.observer.velocity = array([0.000000000000001,0.,0.])
     alt, az, d = diff.apparent().altaz()
     print dt, altitude, alt.degrees()
-    #
-    # Does spaceweather.com have different orbital elements today from
-    # the ones I can download from Celetrak?  The transits are several
-    # minutes off.  Maybe I should find another data source.  For now:
-    # just be happy if we think the ISS is above the horizon when
-    # spaceweather thinks it is transiting!
-    #
-    assert alt > 0
+    assert abs(alt.degrees() - altitude) < 2.0  # TODO: tighten this up?
