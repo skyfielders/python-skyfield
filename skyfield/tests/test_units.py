cc23b97c12ab6001f2fb9e953a9bb74fbe288e76
Brandon Rhodes <brandon@rhodesmill.org>
2014-02-08 17:33:45 +0000

    Give measurements a good exception when unpacked

--- skyfield/tests/test_units.py
+++ skyfield/tests/test_units.py
@@ -8,8 +8,19 @@ try:
 except ImportError:
     u = None
 
-pytestmark = pytest.mark.skipif(u is None, reason='cannot import AstroPy')
+needs_astropy = pytest.mark.skipif(u is None, reason='cannot import AstroPy')
 
+def test_iterating_over_raw_measurement():
+    distance = units.Distance(AU=1.234)
+    with pytest.raises(units.UnpackingError):
+        x, y, z = distance
+
+def test_iterating_over_raw_velocity():
+    velocity = units.Velocity(AU_per_d=1.234)
+    with pytest.raises(units.UnpackingError):
+        x, y, z = velocity
+
+@needs_astropy
 def test_converting_distance_with_astropy():
     distance = units.Distance(AU=1.234)
     value1 = distance.km
@@ -17,6 +28,7 @@ def test_converting_distance_with_astropy():
     epsilon = 0.02         # definitions of AU seem to disagree slightly
     assert abs(value1 - value2.value) < epsilon
 
+@needs_astropy
 def test_converting_velocity_with_astropy():
     velocity = units.Velocity(AU_per_d=1.234)
     value1 = velocity.km_per_s
--- skyfield/units.py
+++ skyfield/units.py
@@ -1,6 +1,16 @@
 from .constants import AU_KM, DAY_S
 
+class UnpackingError(Exception):
+    """You cannot iterate directly over a Skyfield measurement object."""
+
 class Distance(object):
+    """A distance, stored internally as AU and available in other units.
+
+    You can initialize a ``Distance`` by providing a single float or a
+    float array as either an ``AU=`` parameter or a ``km=`` parameter
+    when building a ``Distance`` object.
+
+    """
     def __init__(self, AU=None, km=None):
         if AU is not None:
             self.AU = AU
@@ -17,11 +27,23 @@ class Distance(object):
     def __str__(self):
         return '%s AU' % self.AU
 
+    def __iter__(self):
+        raise UnpackingError(_iter_message % {
+            'class': self.__class__.__name__, 'values': 'x, y, z',
+            'attr1': 'AU', 'attr2': 'km'})
+
     def to(self, unit):
+        """Return this distance in the given AstroPy units."""
         from astropy.units import AU
         return (self.AU * AU).to(unit)
 
 class Velocity(object):
+    """A velocity, stored internally as AU/day and available in other units.
+
+    You can initialize a ``Velocity`` by providing a single float or a
+    float array as either an ``AU_per_d=`` parameter.
+
+    """
     def __init__(self, AU_per_d):
         self.AU_per_d = AU_per_d
 
@@ -31,6 +53,23 @@ class Velocity(object):
             return self.km_per_s
         raise AttributeError('no attribute named %r' % (name,))
 
+    def __iter__(self):
+        raise UnpackingError(_iter_message % {
+            'class': self.__class__.__name__, 'values': 'xdot, ydot, zdot',
+            'attr1': 'AU_per_d', 'attr2': 'km_per_s'})
+
     def to(self, unit):
+        """Return this velocity in the given AstroPy units."""
         from astropy.units import AU, d
         return (self.AU_per_d * AU / d).to(unit)
+
+_iter_message = """\
+cannot directly unpack a %(class)s into several values
+
+To unpack a %(class)s into three components, you need to ask for its
+value in specific units through an attribute or method:
+
+    %(values)s = velocity.%(attr1)s
+    %(values)s = velocity.%(attr2)s
+    %(values)s = velocity.to(astropy_unit)
+"""
