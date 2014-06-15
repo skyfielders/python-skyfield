679d3a27962b0baa79dede4f1a6465160cfa76ac
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-15 11:15:06 -0400

    Advertise troves for Python 2.6 through 3.4

--- setup.py
+++ setup.py
@@ -16,8 +16,11 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Astronomy',
         ],
     packages=[
