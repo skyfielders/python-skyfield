63030751b38777cd10349e99bec02304fc6e7f78
Brandon Rhodes <brandon@rhodesmill.org>
2014-08-18 18:08:12 -0400

    Experiment with a version-info tuple

--- skyfield/__init__.py
+++ skyfield/__init__.py
@@ -4,4 +4,5 @@ Most users will use Skyfield by importing ``skyfield.api`` and using the
 functions and classes there.
 
 """
-__version__ = '0.2'
+__version_info__ = (0, 2)
+__version__ = '%s.%s' % __version_info__
