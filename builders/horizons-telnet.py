8a5da2d16c328125f09cbdb9b6eae00ac2534159
Brandon Rhodes <brandon@rhodesmill.org>
2015-01-25 13:19:59 -0500

    Automate "telnet" to HORIZONS using Makefile

--- builders/horizons-telnet.py
+++ builders/horizons-telnet.py
@@ -3,17 +3,22 @@
 #import argparse
 from telnetlib import Telnet
 
-def main(path):
-    with open(path) as f:
+def main(in_path, out_path):
+    with open(in_path) as f:
         lines = f.read().split('\n')
     tn = Telnet('horizons.jpl.nasa.gov', 6775)
-    out = open('output.txt', 'w')
+    out = open(out_path, 'w')
     for line in lines:
+        print(repr(line))
         tn.write(line + '\r\n')
-        data = tn.read_until('DUMMY PATTERN', 5.0)
+        data = tn.read_until('DUMMY PATTERN', 2.0)
+        print(data)
         out.write(data)
         out.flush()
 
 if __name__ == '__main__':
-    import sys
-    main(sys.argv[1])
+    try:
+        main('horizons-input.txt', 'horizons-output.txt')
+    except EOFError:
+        print
+        print('EOF')
