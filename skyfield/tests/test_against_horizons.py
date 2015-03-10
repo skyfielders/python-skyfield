5a92b79eb38369e1c987d19cbdfe81f284566238
Brandon Rhodes <brandon@rhodesmill.org>
2015-03-10 00:46:18 -0400

    Reduce geometric error to error in Boston position
    Why HORIZONS appears to disagree with us on the position of Boston is,
    of course, still open to investigation.

--- skyfield/tests/test_against_horizons.py
+++ skyfield/tests/test_against_horizons.py
@@ -50,11 +50,15 @@ def other():
     jd = api.JulianDate(tdb=(2015, 3, 2), delta_t=67.185390)
     bos = api.earth.topos(
         '42.3567000 N', '288.943100 E') #, elevation_m=92.0)
-    a = bos(jd).observe(api.earth)
+
+    a = api.earth(jd) - bos(jd) #bos(jd).observe(api.earth)
     v = a.position.au
     print('usno', [repr(n) for n in v / meter])
+
     from skyfield.functions import length_of
     print('r = ', length_of(v) / meter)
+    from ..constants import C
+    print('r(c) = ', length_of(v) / meter / C)
     from skyfield.constants import ERAD
     print('R = ', ERAD)
 
@@ -63,20 +67,21 @@ def off_test_boston_geometry():
     other()
     print('-------')
     k = Kernel(download(de430_url))
-    boston = api.Topos((42, 21, 24.1), (-71, 3, 24.8))
+    #boston = api.Topos((42, 21, 24.1), (-71, 3, 24.8))
     boston = api.Topos('42.3567000 N', '288.943100 E', elevation_m=0.0)#1.014E-9)
     #print(boston.longitude.degrees+360.0)
     #print(boston.observe(k.moon))
     #print(boston.observe(k.earth))
     jd = api.JulianDate(tdb=(2015, 3, 2), delta_t=67.185390)
-    a = boston.observe(k.earth).at(tdb=(2015, 3, 2))
+    #a = boston.observe(k.earth).at(tdb=(2015, 3, 2))
     a = boston.geometry_of(k.earth).at(jd) #tdb=(2015, 3, 2, 0, -1, -7.71))
     #print(a.jd.tdb)
     print('got ', [repr(n) for n in (a.position.au) / meter])
     from numpy import array
     w = array([-1.179627735416499E-06, -3.153207525942794E-05, -2.857611787776292E-05])
     print('want', [repr(n) for n in (w) / meter])
-    print([repr(n) for n in (a.position.au - w) / meter])
+    print('diff', [repr(n) for n in (a.position.au - w) / meter])
+    #print('at() light time:', a.lighttime)
     from skyfield.functions import length_of
     print(length_of(a.position.au) / meter)
     print(length_of(w) / meter)
@@ -91,11 +96,15 @@ def off_test_moon_from_boston_geometry():
     boston = api.Topos('42.3567000 N', '288.943100 E')
     #print(boston.longitude.degrees+360.0)
     #print(boston.observe(k.moon))
-    a = boston.observe(k.moon).at(tdb=(2015, 3, 2))
-    print(a.position.au)
+    jd = api.JulianDate(tdb=(2015, 3, 2), delta_t=67.185390)
+    #a = boston.observe(k.moon).at(jd)
+    a = boston.geometry_of(k.moon).at(jd)
+    diff = a.position.au - [-1.341501206552443E-03, 2.190483327459023E-03, 6.839177007993498E-04]
+    print('diff (au)', [repr(n) for n in diff])
+    print('diff (m) ', [repr(n) for n in diff * AU_M])
     #
     compare(a.position.au,
-      [-1.341501206885335E-03, 2.281775998031755E-03, -2.438420054070379E-04],
+      [-1.341501206552443E-03, 2.190483327459023E-03, 6.839177007993498E-04],
       0.001 * meter)
 
 def off_test_moon_from_boston_astrometric():
