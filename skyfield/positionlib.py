591de4b6a0f756eb5376644b7a3e2bc47a70ab7c
Brandon Rhodes <brandon@rhodesmill.org>
2016-03-20 20:56:39 -0400

    Remove old unused legacy copy of Topos class

--- skyfield/positionlib.py
+++ skyfield/positionlib.py
@@ -4,14 +4,12 @@ from numpy import array, einsum, exp
 
 from .constants import RAD2DEG, TAU
 from .data.spice import inertial_frames
-from .errors import raise_error_for_deprecated_time_arguments
 #from .framelib import ICRS_to_J2000
-from .functions import from_polar, length_of, to_polar, rot_y, rot_z
-from .earthlib import compute_limb_angle, refract, terra
+from .functions import from_polar, length_of, to_polar, rot_z
+from .earthlib import compute_limb_angle, refract
 from .relativity import add_aberration, add_deflection
 from .timelib import JulianDate
-from .units import (Distance, Velocity, Angle, _interpret_angle,
-                    _interpret_ltude)
+from .units import Distance, Velocity, Angle, _interpret_angle
 
 _ECLIPJ2000 = inertial_frames['ECLIPJ2000']
 _GALACTIC = inertial_frames['GALACTIC']
@@ -149,75 +147,6 @@ class ICRS(object):
         p = from_polar(r, alt, az)
         p = einsum('ji...,j...->i...', R, p)
         return Apparent(p)
-
-
-class Topos(object):
-    """An object representing a specific location on the Earth's surface."""
-
-    def __init__(self, latitude=None, longitude=None, latitude_degrees=None,
-                 longitude_degrees=None, elevation_m=0.0):
-
-        if latitude_degrees is not None:
-            latitude = Angle(degrees=latitude_degrees)
-        elif isinstance(latitude, str):
-            latitude = _interpret_ltude(latitude, 'latitude', 'N', 'S')
-        elif not isinstance(latitude, Angle):
-            raise TypeError('please provide either latitude_degrees=<float>'
-                            ' or latitude=<skyfield.units.Angle object>'
-                            ' with north being positive')
-
-        if longitude_degrees is not None:
-            longitude = Angle(degrees=longitude_degrees)
-        elif isinstance(longitude, str):
-            longitude = _interpret_ltude(longitude, 'longitude', 'E', 'W')
-        elif not isinstance(longitude, Angle):
-            raise TypeError('please provide either longitude_degrees=<float>'
-                            ' or longitude=<skyfield.units.Angle object>'
-                            ' with east being positive')
-
-        self.latitude = latitude
-        self.longitude = longitude
-        self.elevation = Distance(m=elevation_m)
-
-        self.R_lat = rot_y(latitude.radians)[::-1]
-
-    def __call__(self, jd):
-        """Compute where this Earth location was in space on a given date."""
-        e = self.ephemeris.earth(jd)
-        tpos_au, tvel_au_per_d = self._position_and_velocity(jd)
-        t = Barycentric(e.position.au + tpos_au,
-                        e.velocity.au_per_d + tvel_au_per_d,
-                        jd)
-        t.geocentric = False  # test, then get rid of this attribute
-        t.rGCRS = tpos_au
-        t.vGCRS = tvel_au_per_d
-        t.topos = self
-        t.ephemeris = self.ephemeris
-        t.altaz_rotation = self._altaz_rotation(jd)
-        return t
-
-    @raise_error_for_deprecated_time_arguments
-    def gcrs(self, jd):
-        """Compute where this location was in the GCRS on a given date."""
-        tpos_au, tvel_au_per_d = self._position_and_velocity(jd)
-        t = Geocentric(tpos_au, tvel_au_per_d, jd)
-        t.topos = self
-        t.ephemeris = self.ephemeris
-        t.altaz_rotation = self._altaz_rotation(jd)
-        return t
-
-    def _position_and_velocity(self, jd):
-        """Return the GCRS position, velocity of this Topos at `jd`."""
-        pos, vel = terra(self.latitude.radians, self.longitude.radians,
-                         self.elevation.au, jd.gast)
-        pos = einsum('ij...,j...->i...', jd.MT, pos)
-        vel = einsum('ij...,j...->i...', jd.MT, vel)
-        return pos, vel
-
-    def _altaz_rotation(self, jd):
-        """Compute the rotation from the ICRS into the alt-az system."""
-        R_lon = rot_z(- self.longitude.radians - jd.gast * TAU / 24.0)
-        return einsum('ij...,jk...,kl...->il...', self.R_lat, R_lon, jd.M)
 
 
 class Barycentric(ICRS):
