d6aa13756c0a9030dc43ce8b9ae1d3e973b46142
Brandon Rhodes <brandon@rhodesmill.org>
2016-03-24 00:30:00 -0400

    Version 0.7

--- skyfield/__init__.py
+++ skyfield/__init__.py
@@ -5,5 +5,5 @@ functions and classes there.  See the ``documentation`` directory inside
 the source code, as well as the http://rhodesmill.org/skyfield/ site!
 
 """
-VERSION = (0, 6, 1)
+VERSION = (0, 7)
 __version__ = '.'.join(map(str, VERSION))
