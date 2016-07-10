bd69c762dce7dfca427468f7822dc39b2cc0f008
Brandon Rhodes <brandon@rhodesmill.org>
2016-07-09 21:17:51 -0400

    Avoid installing astropy under Python <2.7
    This will hopefully help the Travis CI build.

--- requirements.sh
+++ requirements.sh
@@ -6,7 +6,10 @@ if conda --version >/dev/null 2>&1
 then
     conda install astropy mock numpy sphinx pytz lxml html5lib beautifulsoup4
 else
-    pip install astropy mock numpy sphinx pytz
+    if ! python --version | grep -q 2.6
+    then pip install astropy
+    fi
+    pip install mock numpy sphinx pytz
 fi
 pip install de405==1997.1 de423==2010.1
 pip install https://github.com/brandon-rhodes/assay/archive/master.zip
