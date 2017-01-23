0dda6ba15b050304d3ea05e83be8b208e1b894ce
Brandon Rhodes <brandon@rhodesmill.org>
2017-01-23 06:44:15 -0500

    Start testing how successful vector add works

--- skyfield/tests/test_vectors.py
+++ skyfield/tests/test_vectors.py
@@ -16,3 +16,34 @@ def test_bad_subtraction():
     usno = Topos('38.9215 N', '77.0669 W', elevation_m=92.0)
     with assert_raises(ValueError, 'if they both start at the same center'):
         earth - usno
+
+def test_vectors():
+    ts = load.timescale()
+    t = ts.tt(2017, 1, 23, 10, 44)
+
+    planets = load('de421.bsp')
+    earth = planets['earth']
+    moon = planets['moon']
+    mars = planets['mars']
+
+    v = earth
+
+    assert str(v) == """\
+Sum of 2 vectors:
+ + Segment 'de421.bsp' 0 SOLAR SYSTEM BARYCENTER -> 3 EARTH BARYCENTER
+ + Segment 'de421.bsp' 3 EARTH BARYCENTER -> 399 EARTH"""
+
+    assert str(v.at(t)) == "\
+<Barycentric position and velocity at date t center=0 target=399>"
+
+    v = earth - mars
+
+    assert str(v) == """\
+Sum of 4 vectors:
+ + Segment 'de421.bsp' 0 SOLAR SYSTEM BARYCENTER -> 3 EARTH BARYCENTER
+ + Segment 'de421.bsp' 3 EARTH BARYCENTER -> 399 EARTH
+ - Segment 'de421.bsp' 0 SOLAR SYSTEM BARYCENTER -> 4 MARS BARYCENTER
+ - Segment 'de421.bsp' 4 MARS BARYCENTER -> 499 MARS"""
+
+    assert str(v.at(t)) == "\
+<Geocentric position and velocity at date t center=399 target=499>"
