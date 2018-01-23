5374f346ad6e05ff6b0a1e15bff1b56a663d268f
Brandon Rhodes <brandon@rhodesmill.org>
2018-01-23 08:04:02 -0500

    Properly split test into n tests

--- skyfield/tests/test_topos.py
+++ skyfield/tests/test_topos.py
@@ -3,18 +3,19 @@ from numpy import abs
 from skyfield.api import load
 from skyfield.toposlib import Topos
 
+angle = (15, 25, 35, 45)
+
 def ts():
     yield load.timescale()
 
-def test_beneath(ts):
+def test_beneath(ts, angle):
     t = ts.utc(2018, 1, 19, 14, 37, 55)
-    for deg in 15, 25, 35, 45:
-        # An elevation of 0 is more difficult for the routine's accuracy
-        # than a very large elevation.
-        top = Topos(latitude_degrees=deg, longitude_degrees=0, elevation_m=0)
-        p = top.at(t)
-        b = Topos.subpoint(p)
-        error_degrees = abs(b.latitude.degrees - deg)
-        error_mas = 60.0 * 60.0 * 1000.0 * error_degrees
-        #print(b.latitude.degrees, deg, error_mas)
-        assert error_mas < 0.1
+    # An elevation of 0 is more difficult for the routine's accuracy
+    # than a very large elevation.
+    top = Topos(latitude_degrees=angle, longitude_degrees=0, elevation_m=0)
+    p = top.at(t)
+    b = Topos.subpoint(p)
+    error_degrees = abs(b.latitude.degrees - angle)
+    error_mas = 60.0 * 60.0 * 1000.0 * error_degrees
+    #print(b.latitude.degrees, deg, error_mas)
+    assert error_mas < 0.1
