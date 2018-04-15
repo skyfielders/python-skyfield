skyfielders/python-skyfield

commit 527cfb00065aeb1a9dbc8d9c7c5000c2fbe9d810

Allow Empty Time and Angle Objects (#152)
* Fix Time __repr__

The only obstacle to having an empty time was that in a time object the __repr__ couldn't handle when self.tt was an empty array.

* Allow Angles to be Initialized with Lists

Makes it possible to instantiate an Angle object by providing the keywords radians, degrees, or hours with a python list of floats. The Distance and Velocity objects currently already have this capability.

* Allow Empty Angle Objects

Fixes the dstr and hstr methods of Angle objects to still work when self.radians is an empty array.

* Fix Failing Tests, Make String Representations Consistent

Fixed the failing tests, also made the __str__ representation consistent with Distance and Velocity objects. Also made the hstr and dstr for empty angles consistent with the hstr and dstr for non-empty angles.

skyfield/units.py
@@ -12,6 +12,8 @@ def _to_array(value):
     """As a convenience, turn Python lists and tuples into NumPy arrays."""
     if isinstance(value, (tuple, list)):
         return array(value)
+    elif isinstance(value, (float, int)):
+        return np.float64(value)
     else:
         return value
 
@@ -144,7 +146,8 @@ Angle(radians=value)
 Angle(degrees=value)
 Angle(hours=value)
 
-where `value` can be either a Python float or a NumPy array of floats"""
+where `value` can be either a Python float, a list of Python floats, 
+or a NumPy array of floats"""
 
 class Angle(object):
 
@@ -156,12 +159,12 @@ class Angle(object):
                 raise ValueError(_instantiation_instructions)
             self.radians = angle.radians
         elif radians is not None:
-            self.radians = radians
+            self.radians = _to_array(radians)
         elif degrees is not None:
-            self._degrees = degrees = _unsexagesimalize(degrees)
+            self._degrees = degrees = _unsexagesimalize(_to_array(degrees))
             self.radians = degrees * _from_degrees
         elif hours is not None:
-            self._hours = hours = _unsexagesimalize(hours)
+            self._hours = hours = _unsexagesimalize(_to_array(hours))
             self.radians = hours * _from_hours
 
         self.preference = (preference if preference is not None
@@ -189,10 +192,15 @@ class Angle(object):
         raise AttributeError('no attribute named %r' % (name,))
 
     def __str__(self):
+        if self.radians.size == 0:
+            return 'Angle []'
         return self.dstr() if self.preference == 'degrees' else self.hstr()
 
     def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, self)
+        if self.radians.size == 0:
+            return '<{0} []>'.format(type(self).__name__, self)
+        else:
+            return '<{0} {1}>'.format(type(self).__name__, self)
 
     def hms(self, warn=True):
         """Convert to a tuple (hours, minutes, seconds).
@@ -220,6 +228,8 @@ class Angle(object):
         """Convert to a string like ``12h 07m 30.00s``."""
         if warn and self.preference != 'hours':
             raise WrongUnitError('hstr')
+        if self.radians.size == 0:
+            return '<Angle []>'
         hours = self._hours
         if getattr(hours, 'shape', ()):
             return "{0} values from {1} to {2}".format(
@@ -255,6 +265,8 @@ class Angle(object):
         """Convert to a string like ``181deg 52\' 30.0"``."""
         if warn and self.preference != 'degrees':
             raise WrongUnitError('dstr')
+        if self.radians.size == 0:
+            return '<Angle []>'
         degrees = self._degrees
         signed = self.signed
         if getattr(degrees, 'shape', ()):

skyfield/timelib.py
@@ -218,11 +218,14 @@ class Time(object):
         return self.shape[0]
 
     def __repr__(self):
-
-        if getattr(self.tt, 'shape', ()):
+        size = getattr(self.tt, 'size', -1)
+        if size > 1:
             rstr = '<Time {0} values from tt={1:.6f} to tt={2:.6f}>'
             return rstr.format(self.tt.size, self.tt.min(), self.tt.max())
-        return '<Time tt={0:.6f}>'.format(self.tt)
+        elif size == 0:
+            return '<Time []>'
+        else:
+            return '<Time tt={0:.6f}>'.format(self.tt)
 
     def __getitem__(self, index):
         # TODO: also copy cached matrices?

commit ee29373da80ca8b3b417d8e1afb0ee10259b8268

Fix ability to build angle from (d,m,s) or (h,m,s)
This was broken in #152 527cfb00065a because I had failed to write unit
tests to preserve the intended behavior.

skyfield/units.py
@@ -161,10 +161,10 @@ class Angle(object):
         elif radians is not None:
             self.radians = _to_array(radians)
         elif degrees is not None:
-            self._degrees = degrees = _unsexagesimalize(_to_array(degrees))
+            self._degrees = degrees = _to_array(_unsexagesimalize(degrees))
             self.radians = degrees * _from_degrees
         elif hours is not None:
-            self._hours = hours = _unsexagesimalize(_to_array(hours))
+            self._hours = hours = _to_array(_unsexagesimalize(hours))
             self.radians = hours * _from_hours
 
         self.preference = (preference if preference is not None
@@ -231,7 +231,8 @@ class Angle(object):
         if self.radians.size == 0:
             return '<Angle []>'
         hours = self._hours
-        if getattr(hours, 'shape', ()):
+        shape = getattr(hours, 'shape', ())
+        if shape and shape != (1,):
             return "{0} values from {1} to {2}".format(
                 len(hours),
                 _hstr(min(hours), places),
@@ -269,7 +270,8 @@ class Angle(object):
             return '<Angle []>'
         degrees = self._degrees
         signed = self.signed
-        if getattr(degrees, 'shape', ()):
+        shape = getattr(degrees, 'shape', ())
+        if shape and shape != (1,):
             return "{0} values from {1} to {2}".format(
                 len(degrees),
                 _dstr(min(degrees), places, signed),

skyfield/tests/test_angles.py
@@ -37,3 +37,13 @@ def test_angle_array_strs():
     assert str(Angle(hours=np.array([11, 12, 13]))) == (
         '''3 values from 11h 00m 00.00s to 13h 00m 00.00s'''
         )
+
+def test_angle_sexagesimal_args():
+    assert str(Angle(degrees=(90,))) == '''90deg 00' 00.0"'''
+    assert str(Angle(hours=(12,))) == '''12h 00m 00.00s'''
+
+    assert str(Angle(degrees=(90, 15))) == '''90deg 15' 00.0"'''
+    assert str(Angle(hours=(12, 30))) == '''12h 30m 00.00s'''
+
+    assert str(Angle(degrees=(90, 15, 30))) == '''90deg 15' 30.0"'''
+    assert str(Angle(hours=(12, 30, 15))) == '''12h 30m 15.00s'''