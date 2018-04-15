8033ba887ae10a78c1f5d60ac490794c5e7af4da
Brandon Rhodes <brandon@rhodesmill.org>
2018-04-15 18:34:00 -0400

    Make life more stable by specifying versions

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,16 @@ set -e
 
 if conda --version >/dev/null 2>&1
 then
-    conda install astropy mock numpy sphinx pytz lxml html5lib beautifulsoup4
+    conda install \
+          astropy=3.0.1 \
+          mock=2.0.0 \
+          numpy=1.14.2 \
+          sphinx=1.7.2 \
+          pytz \
+          lxml=4.2.1 \
+          html5lib=1.0.1 \
+          beautifulsoup4=4.6.0 \
+
 else
     if ! python --version 2>&1 | grep -q 2.6
     then pip install astropy
