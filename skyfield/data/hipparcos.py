3df2f6218337bde41bf215c86c3c2cba37e15c84
Brandon Rhodes <brandon@rhodesmill.org>
2018-06-02 15:31:43 -0400

    Load the Hipparcos catalog as a Pandas DataFrame

--- skyfield/data/hipparcos.py
+++ skyfield/data/hipparcos.py
@@ -33,6 +33,52 @@ def load(match_function):
             if match_function(line):
                 yield parse(line)
 
+def load_dataframe(path):
+    from pandas import read_fwf
+    # from csv import reader
+
+    # with api.load.open(url) as f:
+    #     df = read_csv(f, sep='|', compression='gzip')
+
+    from time import time
+    t0 = time()
+
+    # with api.load.open(url) as f:
+    #     #for line in reader(gzip.open(f, 'rt', encoding='ascii')):
+    #     for line in gzip.open(f, 'rt', encoding='ascii'):
+    #         # assert len(line) >= 43, repr(line)
+    #         assert line[43] == '.', (repr(line), repr(line[43]))
+    #         if match_function(line):
+    #             yield parse(line)
+
+    names, colspecs = zip(
+        ('hip', (2, 14)),
+        ('magnitude', (41, 46)),
+        ('ra_hours', (51, 63)),
+        ('dec_degrees', (64, 76)),
+    )
+
+    #with api.load.open(url) as f:
+    with open(path, 'rb') as f:
+        with gzip.open(f, 'rt', encoding='ascii') as g:
+        # with gzip.open(f, 'rb') as g:
+            #print(g)
+            df = read_fwf(g, names=names, colspecs=colspecs)
+
+    #'H|         104| |00 01 18.42|-31 53 58.6| 9.'
+    #print(df.head(1))
+    print(time() - t0)
+    df = df.set_index('hip')
+    # print(df.head())
+    # print(df.info())
+    # print(df.magnitude.isnull().sum())
+    # print(df.ra_hours.isnull().sum())
+    # print(df.dec_degrees.isnull().sum())
+        # for line in gzip.GzipFile(fileobj=f):
+        #     if match_function(line):
+        #         yield parse(line)
+    return df
+
 def get(which):
     """Return a single star, or a list of stars, from the Hipparcos catalog.
 
