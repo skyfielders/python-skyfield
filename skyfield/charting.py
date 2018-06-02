ac7662e16003ffd6d5d40718aad9656e8df5e729
Brandon Rhodes <brandon@rhodesmill.org>
2018-06-02 19:00:10 -0400

    More playing with magnitude

--- skyfield/charting.py
+++ skyfield/charting.py
@@ -37,20 +37,16 @@ def plot_stars(catalog, observer, project, mag1, mag2, ax):
     s = Star(ra_hours=c.ra_hours, dec_degrees=c.dec_degrees)
     spos = o.observe(s)
     x, y = project(spos)
-    mmin = c['magnitude'].min()
-    mmax = c['magnitude'].max()
-    m = (mmax - c['magnitude']) / (mmax - mmin)
-    size = 1.0 + 10.0 * m
-    size **= 1.8
+    scale = 2.0
+    size = ((mag1 - c['magnitude']) * scale) ** 2.0
     ax.scatter(x, y, s=size, c='k')
 
     c = catalog
-    c = c[c['magnitude'] >= mag1]
+    c = c[c['magnitude'] > mag1]
     c = c[c['magnitude'] <= mag2]
     s = Star(ra_hours=c.ra_hours, dec_degrees=c.dec_degrees)
     spos = o.observe(s)
     x, y = project(spos)
     m = (mag2 - c['magnitude']) / (mag2 - mag1)
-    size = 0.1 + m * 0.9
     # Note that "gray_r" is white for 0.0 and black for 1.0
-    ax.scatter(x, y, s=1, c=m, cmap='gray_r', vmin=0.0, vmax=1.0)
+    ax.scatter(x, y, s=1.0, c=0.1 + 0.8 * m, cmap='gray_r', vmin=0.0, vmax=1.0)
