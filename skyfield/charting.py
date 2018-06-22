434be37e50265a9a2c34270a625e34fcb7413ad3
Brandon Rhodes <brandon@rhodesmill.org>
2018-06-22 11:32:39 -0400

    Fix date rendering; switch planet to scatter()

--- skyfield/charting.py
+++ skyfield/charting.py
@@ -42,7 +42,7 @@ def _plot_stars(catalog, observer, project, ax, mag1, mag2, margin=1.25):
     c = catalog
     c = c[c['magnitude'] > mag1]
     c = c[c['magnitude'] <= mag2]
-    print('Second star group:', len(c))
+    #print('Second star group:', len(c))
     c = c.sort_values('magnitude', ascending=False)
     s = Star(ra_hours=c.ra_hours, dec_degrees=c.dec_degrees)
     spos = o.observe(s)
@@ -58,25 +58,27 @@ def _plot_stars(catalog, observer, project, ax, mag1, mag2, margin=1.25):
     # white gap in case stars are touching.  Draw the brightest stars
     # first to stop them from completely occluding smaller companions.
 
+    def mag_to_radius(m):
+        return (mag1 - m) * scale + 1.0
+
     c = catalog
     c = c[c['magnitude'] <= mag1]
     c = c.sort_values('magnitude', ascending=True)
-    print('First star group:', len(c))
+    #print('First star group:', len(c))
     s = Star(ra_hours=c.ra_hours, dec_degrees=c.dec_degrees)
     spos = o.observe(s)
     x, y = project(spos)
     scale = 1.5
-    radius = (mag1 - c['magnitude']) * scale + 1.0
+    radius = mag_to_radius(c['magnitude'])
 
     x2 = np.repeat(x, 2)
     y2 = np.repeat(y, 2)
     radius2 = (radius[:,None] + (3.0,0.0)).flatten()
-    #print(size2[:20])
     c2 = ('w', 'k')
 
     art.append(ax.scatter(x2, y2, s=radius2 ** 2.0, c=c2))
 
-    return art
+    return art, mag_to_radius
 
 @writers.register('ffmpeg_small')
 class _FFMpegWriter(FFMpegWriter):
@@ -100,6 +102,17 @@ class _Animation(FuncAnimation):
             self.patcher = patch('matplotlib.backends.backend_agg'
                             '.FigureCanvasAgg.draw', draw)
             self.patcher.__enter__()
+
+    # Experiment to avoid drawing month names over each other when they
+    # are outside the axis borders and don't get blitted:
+
+    # def _pre_draw(self, framedata, blit):
+    #     artists = self._drawn_artists
+    #     for figure in set(a.axes.figure for a in artists):
+    #         from IPython.core.debugger import set_trace
+    #         #set_trace()
+    #         #figure.canvas.renderer.clear()
+    #     super(_Animation, self)._pre_draw(framedata, blit)
 
 def draw(self):
     pass
@@ -129,9 +142,11 @@ def _animate(projection, t, stars, observer, planet):
 
     fig.set_size_inches(9, 9)
 
+    # Draw invisibly the entire path, so that the bounding boxes are
+    # known ahead of time.
     ax.scatter(x, y, color='b', alpha=0.0)
 
-    _plot_stars(stars, o, project, ax, 6.0, 8.5, 0.8)
+    art, mag_to_radius = _plot_stars(stars, o, project, ax, 6.0, 8.5, 0.8)
 
     # http://aa.usno.navy.mil/software/mica/USNO-AA-TN-2003-04.pdf
     # m - M = 5 (log10 d - 1)
@@ -142,46 +157,43 @@ def _animate(projection, t, stars, observer, planet):
     text_x = x_left
     text_y = y_bottom - (y_top - y_bottom) * 0.01
 
-    # date_text.set_clip_on(False)
-    # date_text = fig.text(0.1, 0.1, 'date',
-    #                  ha='center',
-    #                  va='center',
-    #                  transform = ax.transAxes)
-    # date_text.set_clip_on(False)
-    # date_text = fig.text(0.2, 0.2, 'date',
-    #                  ha='center',
-    #                  va='center',
-    #                  transform = ax.transAxes)
-
     saturn_color = '#a69276'  # chroma('#d8c2a5').darken().hex()
 
     planet_art = None
     date_text = None
 
+    # Somehow matplotlib is (a) drawing the date text twice which ruins
+    # the anti-aliasing and makes it look blocky, and (b) also never
+    # erasing that part of the figure so the month names all pile up on
+    # top of each other.  So let's manually paint a white rectangle
+    # behind the text each time to overwrite the previous text.
+    r = ax.add_patch(plt.Rectangle(
+        (x_left, y_bottom - 0.012), 0.08, 0.01,
+        clip_on=False, facecolor='white',
+    ))
+
     def init():
         nonlocal planet_art, date_text
-        planet_art, = ax.plot(x[100], y[100], color=saturn_color, marker='o')
+        print('init()')
+        planet_art = ax.scatter(x[100], y[100], color=saturn_color)
+        #print(dir(planet_art))
         date_text = ax.text(text_x, text_y, '', ha='left', va='top')
-        return planet_art, #date_text
+        return planet_art, r, #date_text
 
     def update(i):
-        planet_art.set_xdata(x[i])
-        planet_art.set_ydata(y[i])
+        #print('Frame {}'.format(i))
 
-        scale = 2.0
-        radius = (6.0 - mag[i]) * scale
+        # planet_art.set_xdata(x[i])
+        # planet_art.set_ydata(y[i])
+        planet_art.set_offsets(((x[i], y[i]),))
+        planet_art.set_sizes((mag_to_radius(mag[i]) ** 2.0,))
 
-        planet_art.set_ms(radius)
-
-        #date_text = ax.text(text_x, text_y, 'date', ha='left', va='top')
         date_text.set_text(t[i].utc_strftime('%Y %B'))
 
-        if i == 0:
-            return planet_art,
-        return planet_art, date_text
+        return planet_art, r, date_text
 
     frames = len(x)
-    frames = 30
+    #frames = 90
 
     anim = _Animation(fig, update, frames=frames,
                       blit=True, init_func=init,
