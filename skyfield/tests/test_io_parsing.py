183b77be89b6b6eccff96ae73565aab6190dce25
Brandon Rhodes <brandon@rhodesmill.org>
2018-07-22 15:12:19 -0400

    Detect satellite names in Spacetrack format
    Fixes #189.

--- skyfield/tests/test_io_parsing.py
+++ skyfield/tests/test_io_parsing.py
@@ -12,9 +12,18 @@ FLOCK 2E-1              \r\n\
 2 41483  51.6270 103.3896 0004826  61.7810 298.3684 15.92672255114129
 """
 
-sample_spacetrack_text = b"""\
+sample_spacetrack_two_line_text = b"""\
 1 29273U 06033B   18081.29838594 -.00000056 +00000-0 +00000-0 0  9993
 2 29273 000.0189 154.5198 0004980 202.4902 284.9321 01.00271755042548
+1 29274U 06033C   18081.39999693 +.00002637 +00000-0 +10299-2 0  9992
+2 29274 005.9144 244.7152 6177908 248.3941 037.5897 03.74556424124616
+"""
+
+sample_spacetrack_three_line_text = b"""\
+0 First
+1 29273U 06033B   18081.29838594 -.00000056 +00000-0 +00000-0 0  9993
+2 29273 000.0189 154.5198 0004980 202.4902 284.9321 01.00271755042548
+0 Second
 1 29274U 06033C   18081.39999693 +.00002637 +00000-0 +10299-2 0  9992
 2 29274 005.9144 244.7152 6177908 248.3941 037.5897 03.74556424124616
 """
@@ -29,8 +38,8 @@ def test_celestrak():
     assert s[1][0] == ['FLOCK 2E-1']
     assert s[1][1].name == 'FLOCK 2E-1'
 
-def test_spacetrack():
-    f = BytesIO(sample_spacetrack_text)
+def test_spacetrack_two_line():
+    f = BytesIO(sample_spacetrack_two_line_text)
     s = list(parse_tle(f))
     assert len(s) == 2
     print(s)
@@ -38,6 +47,16 @@ def test_spacetrack():
     assert s[0][1].name is None
     assert s[1][0] == ()
     assert s[1][1].name is None
+
+def test_spacetrack_three_line():
+    f = BytesIO(sample_spacetrack_three_line_text)
+    s = list(parse_tle(f))
+    assert len(s) == 2
+    print(s)
+    assert s[0][0] == ['First']
+    assert s[0][1].name == 'First'
+    assert s[1][0] == ['Second']
+    assert s[1][1].name == 'Second'
 
 def test_extra_lines_in_tle_are_ignored():
     f = BytesIO(b'Sample line\n' + sample_celestrak_text + b'Another line\n')
