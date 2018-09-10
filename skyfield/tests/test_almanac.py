62c59e1efdd0a5c04bb3854dfb3f98ed5c237c21
Brandon Rhodes <brandon@rhodesmill.org>
2018-09-09 22:10:12 -0400

    Add test for single-value fraction_illuminated()

--- skyfield/tests/test_almanac.py
+++ skyfield/tests/test_almanac.py
@@ -5,5 +5,7 @@ def test_fraction_illuminated():
     ts = api.load.timescale()
     t0 = ts.utc(2018, 9, range(9, 19), 5)
     e = api.load('de421.bsp')
-    p = almanac.fraction_illuminated(e, 'moon', t0).round(2)
-    assert (p == (0, 0, 0.03, 0.08, 0.15, 0.24, 0.33, 0.43, 0.52, 0.62)).all()
+    f = almanac.fraction_illuminated(e, 'moon', t0[-1]).round(2)
+    assert f == 0.62
+    f = almanac.fraction_illuminated(e, 'moon', t0).round(2)
+    assert (f == (0, 0, 0.03, 0.08, 0.15, 0.24, 0.33, 0.43, 0.52, 0.62)).all()
