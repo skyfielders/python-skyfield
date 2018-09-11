2978fca2dcf7ecc6da0c2eeca80c209241f1518e
Brandon Rhodes <brandon@rhodesmill.org>
2018-09-11 04:59:33 -0400

    Add tests for sunrise/sunset and moon quarters

--- skyfield/almanac.py
+++ skyfield/almanac.py
@@ -20,7 +20,7 @@ def phase_angle(ephemeris, body, t):
 
 def fraction_illuminated(ephemeris, body, t):
     a = phase_angle(ephemeris, body, t).radians
-    return 0.5 * (1 + cos(a))
+    return 0.5 * (1.0 + cos(a))
 
 # Search routines.
 
@@ -94,54 +94,20 @@ def _find_maxima(start_time, end_time, f, epsilon=EPSILON, num=12):
 
     return ts.tt_jd(ends), y.take(indices)
 
-def _find_minima(start_time, end_time, f, epsilon=EPSILON, num=12):
-    ts = start_time.ts
-    jd0 = start_time.tt
-    jd1 = end_time.tt
-    if jd0 >= jd1:
-        raise ValueError('your start_time {0} is later than your end_time {1}'
-                         .format(start_time, end_time))
-
-    jd = linspace(jd0, jd1, (jd1 - jd0) / f.rough_period * num // 1.0)
-
-    end_mask = linspace(0.0, 1.0, num)
-    start_mask = end_mask[::-1]
-    o = multiply.outer
-
-    while True:
-        t = ts.tt_jd(jd)
-        y = f(t)
-
-        indices = flatnonzero(diff(sign(diff(y))) == 2)
-        #print(indices)
-        if not len(indices):
-            raise ValueError('cannot find a minimum in that range')
-
-        starts = jd.take(indices)
-        ends = jd.take(indices + 2)
-
-        # Since we start with equal intervals, they all should fall
-        # below epsilon at around the same time; so for efficiency we
-        # only test the first pair.
-        if ends[0] - starts[0] <= epsilon:
-            break
-
-        jd = o(starts, start_mask).flatten() + o(ends, end_mask).flatten()
-
-    return ts.tt_jd(ends), y.take(indices)
+# Discrete circumstances to search.
 
 def sunrise_sunset(ephemeris, topos):
     """Return a function of time that returns whether the sun is up."""
     sun = ephemeris['sun']
     topos_at = (ephemeris['earth'] + topos).at
 
-    def is_sun_up(t):
+    def is_sun_up_at(t):
         """Return `True` if the sun has risen by time `t`."""
         t._nutation_angles = iau2000b(t.tt)
         return topos_at(t).observe(sun).apparent().altaz()[0].degrees > -0.8333
 
-    is_sun_up.rough_period = 0.5  # twice a day
-    return is_sun_up
+    is_sun_up_at.rough_period = 0.5  # twice a day
+    return is_sun_up_at
 
 MOON_QUARTER_NAMES = [
     'New Moon',
@@ -149,8 +115,6 @@ MOON_QUARTER_NAMES = [
     'Full Moon',
     'Last Quarter',
 ]
-
-# Routines for the use of the search functions above.
 
 def moon_quarter(ephemeris):
     """Return a function of time that returns the moon phase 0 through 3."""
@@ -173,8 +137,5 @@ def _distance_to(center, target):
     def distance_at(t):
         t._nutation_angles = iau2000b(t.tt)
         distance = center.at(t).observe(target).distance().au
-        #print(distance)
         return distance
-
-    distance_at.rough_period = 7.0  # one lunar quarter per week
     return distance_at
--- skyfield/tests/test_almanac.py
+++ skyfield/tests/test_almanac.py
@@ -1,11 +1,44 @@
 from skyfield import api, almanac
 
+half_minute = 1.0 / 24.0 / 60.0 / 2.0
+
+# Compare with USNO:
 # http://aa.usno.navy.mil/cgi-bin/aa_moonill2.pl?form=1&year=2018&task=00&tz=-05
+
 def test_fraction_illuminated():
     ts = api.load.timescale()
     t0 = ts.utc(2018, 9, range(9, 19), 5)
     e = api.load('de421.bsp')
-    f = almanac.fraction_illuminated(e, 'moon', t0[-1]).round(2)
-    assert f == 0.62
-    f = almanac.fraction_illuminated(e, 'moon', t0).round(2)
-    assert (f == (0, 0, 0.03, 0.08, 0.15, 0.24, 0.33, 0.43, 0.52, 0.62)).all()
+    i = almanac.fraction_illuminated(e, 'moon', t0[-1]).round(2)
+    assert i == 0.62
+    i = almanac.fraction_illuminated(e, 'moon', t0).round(2)
+    assert (i == (0, 0, 0.03, 0.08, 0.15, 0.24, 0.33, 0.43, 0.52, 0.62)).all()
+
+# Compare with USNO:
+# http://aa.usno.navy.mil/rstt/onedaytable?ID=AA&year=2018&month=9&day=12&state=OH&place=Bluffton
+
+def test_sunrise_sunset():
+    ts = api.load.timescale()
+    t0 = ts.utc(2018, 9, 12, 4)
+    t1 = ts.utc(2018, 9, 13, 4)
+    e = api.load('de421.bsp')
+    bluffton = api.Topos('40.8939 N', '83.8917 W')
+    t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(e, bluffton))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    assert strings == ['2018-09-12 11:13', '2018-09-12 23:50']
+    assert (y == (0, 1)).all()
+
+# Compare with USNO:
+# http://aa.usno.navy.mil/cgi-bin/aa_phases.pl?year=2018&month=9&day=11&nump=50&format=p
+
+def test_moon_quarters():
+    ts = api.load.timescale()
+    t0 = ts.utc(2018, 9, 11)
+    t1 = ts.utc(2018, 9, 30)
+    e = api.load('de421.bsp')
+    t, y = almanac.find_discrete(t0, t1, almanac.moon_quarter(e))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    assert strings == ['2018-09-16 23:15', '2018-09-25 02:52']
+    assert (y == (0, 1)).all()
