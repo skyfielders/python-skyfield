92ea3f03ba4760bbad7efac7514a0e386c4143f4
Brandon Rhodes <brandon@rhodesmill.org>
2019-07-21 14:19:00 -0400

    Symlink hip_main.dat.gz so tests always work

--- skyfield/tests/test_stars.py
+++ skyfield/tests/test_stars.py
@@ -3,7 +3,7 @@ from skyfield.data.hipparcos import load_dataframe
 
 def test_dataframe():
     ts = api.load.timescale(builtin=True)
-    with api.load.open('ci/hip_main.dat.gz') as f:
+    with api.load.open('hip_main.dat.gz') as f:
         df = load_dataframe(f)
     star = api.Star.from_dataframe(df)
     assert repr(star) == 'Star(ra shape=214, dec shape=214, ra_mas_per_year shape=214, dec_mas_per_year shape=214, parallax_mas shape=214, epoch shape=214)'
