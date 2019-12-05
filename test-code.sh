bfbbe25da50a226cd07229934a4d53ed76b629f3
Brandon Rhodes <brandon@rhodesmill.org>
2019-12-05 08:18:04 -0500

    Create test-code.sh command to go with test-docs.sh

---
+++ test-code.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+if ! command -v assaym >/dev/null
+then
+    cat >&2 <<'EOF'
+Error: "assay" command not found
+
+Create a virtual environment and run "pip install -r requirements.txt"
+to install all of the tools and libraries for Skyfield development.
+
+EOF
+    exit 2
+fi
+exec assay --batch skyfield/tests
