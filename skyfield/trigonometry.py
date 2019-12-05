43f5e62f8dd2b4474568f5127297672045449b1e
Brandon Rhodes <brandon@rhodesmill.org>
2019-12-05 18:33:25 -0500

    Improve the docs for position angle

--- skyfield/trigonometry.py
+++ skyfield/trigonometry.py
@@ -7,10 +7,11 @@ from skyfield.units import Angle
 def position_angle_of(latlon1, latlon2):
     """Return the position angle of one position with respect to another.
 
-    Both arguments should be a pair of :class:`~skyfield.units.Angle`
-    objects.  The position angle is 0 degrees if position #2 is directly
-    north of position #1 on the celestial sphere, 90 degrees if east,
-    180 if south, and 270 if west.
+    Both arguments should be a tuple of :class:`~skyfield.units.Angle`
+    objects whose first item is latitude-like and whose second item is
+    longitude-like.  The position angle returned will be 0 degrees if
+    position #2 is directly north of position #1 on the celestial
+    sphere, 90 degrees if east, 180 if south, and 270 if west.
 
     >>> from skyfield.trigonometry import position_angle_of
     >>> from skyfield.units import Angle
@@ -18,6 +19,13 @@ def position_angle_of(latlon1, latlon2):
     >>> b = Angle(degrees=1), Angle(degrees=1)
     >>> position_angle_of(a, b)
     <Angle 315deg 00' 15.7">
+
+    The tuples provided as arguments can have more than 2 items; any
+    extra items are ignored.  This means you can pass in the output of
+    routines like :meth:`~skyfield.positionlib.ICRF.ecliptic_latlon()`
+    and :meth:`~skyfield.positionlib.Apparent.altaz()` as arguments.
+    Their angles will be used, but the third item of each tuple, the
+    distance, will be cleanly ignored.
 
     """
     lat1 = latlon1[0].radians
