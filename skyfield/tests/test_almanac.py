85939e0b7dd96f322ff545fb461c62814fdb7ee0
Mingli Yuan <mountain@users.noreply.github.com>
2019-12-14 00:57:57 +0800

    add test for solar terms

--- skyfield/tests/test_almanac.py
+++ skyfield/tests/test_almanac.py
@@ -96,3 +96,55 @@ def test_close_start_and_end():
     t.tt += half_minute
     strings = t.utc_strftime('%Y-%m-%d %H:%M')
     assert strings == ['2018-09-23 01:54']
+
+# Compare with Hong Kong Observatory:
+# https://www.hko.gov.hk/tc/gts/astronomy/Solar_Term.htm access at 2019-12-14
+
+def test_solar_terms():
+    ts = api.load.timescale()
+    e = api.load('de421.bsp')
+    
+    # https://en.wikipedia.org/wiki/Lichun
+    
+    t0 = ts.utc(2019, 2, 2)
+    t1 = ts.utc(2019, 2, 5)
+    t, y = almanac.find_discrete(t0, t1, almanac.solar_terms(e))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    print(strings)
+    assert strings == ['2019-02-04 19:14']
+    assert (y == (21)).all()
+    
+    # https://en.wikipedia.org/wiki/Lixia
+    
+    t0 = ts.utc(2019, 5, 4)
+    t1 = ts.utc(2019, 5, 7)
+    t, y = almanac.find_discrete(t0, t1, almanac.solar_terms(e))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    print(strings)
+    assert strings == ['2019-05-06 11:03']
+    assert (y == (3)).all()
+    
+    # https://en.wikipedia.org/wiki/Liqiu
+    
+    t0 = ts.utc(2019, 8, 7)
+    t1 = ts.utc(2019, 8, 9)
+    t, y = almanac.find_discrete(t0, t1, almanac.solar_terms(e))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    print(strings)
+    assert strings == ['2019-08-08 11:13']
+    assert (y == (9)).all()
+    
+    # https://en.wikipedia.org/wiki/Lidong
+    
+    t0 = ts.utc(2018, 11, 7)
+    t1 = ts.utc(2018, 11, 9)
+    t, y = almanac.find_discrete(t0, t1, almanac.solar_terms(e))
+    t.tt += half_minute
+    strings = t.utc_strftime('%Y-%m-%d %H:%M')
+    print(strings)
+    assert strings == ['2019-11-08 09:24']
+    assert (y == (15)).all()
+
