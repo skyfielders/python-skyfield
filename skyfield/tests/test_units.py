e21e54707709eb834d58e13d539e2d998a781a74
Brandon Rhodes <brandon@rhodesmill.org>
2020-05-30 16:20:04 -0400

    Move Angle unit tests into main unit test file

--- skyfield/tests/test_units.py
+++ skyfield/tests/test_units.py
@@ -2,7 +2,7 @@
 
 from assay import assert_raises
 from numpy import array
-from skyfield import units
+from skyfield.units import Angle, Distance, Velocity, UnpackingError
 
 try:
     from astropy import units as u
@@ -13,20 +13,67 @@ def needs_astropy(test):
     """Skip `test` if AstroPy is not available."""
     return None if (u is None) else test
 
+def test_degree_rounding():
+    tenth = 0.1 / 60.0 / 60.0  # of an arcsecond
+
+    assert str(Angle(degrees=tenth * -600.75)) == '-00deg 01\' 00.1"'
+    assert str(Angle(degrees=tenth * -600.25)) == '-00deg 01\' 00.0"'
+    assert str(Angle(degrees=tenth * -599.75)) == '-00deg 01\' 00.0"'
+    assert str(Angle(degrees=tenth * -599.25)) == '-00deg 00\' 59.9"'
+
+    assert str(Angle(degrees=tenth * -1.75)) == '-00deg 00\' 00.2"'
+    assert str(Angle(degrees=tenth * -1.25)) == '-00deg 00\' 00.1"'
+    assert str(Angle(degrees=tenth * -0.75)) == '-00deg 00\' 00.1"'
+    assert str(Angle(degrees=tenth * -0.25)) == '-00deg 00\' 00.0"'
+
+    assert str(Angle(degrees=0.0)) == '00deg 00\' 00.0"'
+
+    assert str(Angle(degrees=tenth * 0.25)) == '00deg 00\' 00.0"'
+    assert str(Angle(degrees=tenth * 0.75)) == '00deg 00\' 00.1"'
+    assert str(Angle(degrees=tenth * 1.25)) == '00deg 00\' 00.1"'
+    assert str(Angle(degrees=tenth * 1.75)) == '00deg 00\' 00.2"'
+
+    assert str(Angle(degrees=tenth * 599.25)) == '00deg 00\' 59.9"'
+    assert str(Angle(degrees=tenth * 599.75)) == '00deg 01\' 00.0"'
+    assert str(Angle(degrees=tenth * 600.25)) == '00deg 01\' 00.0"'
+    assert str(Angle(degrees=tenth * 600.75)) == '00deg 01\' 00.1"'
+
+def test_angle_scalar_strs():
+    assert str(Angle(degrees=array(91))) == '''91deg 00' 00.0"'''
+    assert str(Angle(hours=array(12))) == '''12h 00m 00.00s'''
+
+def test_angle_array_strs():
+    assert str(Angle(degrees=array([90, 91, 92]))) == (
+        '''3 values from 90deg 00' 00.0" to 92deg 00' 00.0"'''
+        )
+    assert str(Angle(hours=array([11, 12, 13]))) == (
+        '''3 values from 11h 00m 00.00s to 13h 00m 00.00s'''
+        )
+
+def test_angle_sexagesimal_args():
+    assert str(Angle(degrees=(90,))) == '''90deg 00' 00.0"'''
+    assert str(Angle(hours=(12,))) == '''12h 00m 00.00s'''
+
+    assert str(Angle(degrees=(90, 15))) == '''90deg 15' 00.0"'''
+    assert str(Angle(hours=(12, 30))) == '''12h 30m 00.00s'''
+
+    assert str(Angle(degrees=(90, 15, 30))) == '''90deg 15' 30.0"'''
+    assert str(Angle(hours=(12, 30, 15))) == '''12h 30m 15.00s'''
+
 def test_arcminutes_and_arcseconds_and_mas():
-    angle = units.Angle(degrees=1.0)
+    angle = Angle(degrees=1.0)
     assert angle.arcminutes() == 60
     assert angle.arcseconds() == 60 * 60
     assert angle.mas() == 60 * 60 * 1000
 
 def test_velocity_input_units():
-    v1 = units.Velocity(au_per_d=2.0)
-    v2 = units.Velocity(km_per_s=3462.9137)
+    v1 = Velocity(au_per_d=2.0)
+    v2 = Velocity(km_per_s=3462.9137)
     assert abs(v1.au_per_d - v2.au_per_d) < 1e-7
 
 def test_stringifying_vector_distance():
     a = array([1.23, 4.56])
-    s = str(units.Distance(au=a))
+    s = str(Distance(au=a))
     if '[1' in s:
         # Python 3.5, says Travis CI.  No idea.
         assert s == '[1.23 4.56] au'
@@ -35,26 +82,26 @@ def test_stringifying_vector_distance():
         assert s == '[ 1.23  4.56] au'
 
 def test_iterating_over_raw_measurement():
-    distance = units.Distance(au=1.234)
-    with assert_raises(units.UnpackingError):
+    distance = Distance(au=1.234)
+    with assert_raises(UnpackingError):
         x, y, z = distance
 
 def test_iterating_over_raw_velocity():
-    velocity = units.Velocity(au_per_d=1.234)
-    with assert_raises(units.UnpackingError):
+    velocity = Velocity(au_per_d=1.234)
+    with assert_raises(UnpackingError):
         x, y, z = velocity
 
 def test_converting_from_km_to_m():
-    distance = units.Distance(km=1.234)
+    distance = Distance(km=1.234)
     assert abs(distance.m - 1234.0) < 1e-15
 
 def test_converting_from_m_to_km():
-    distance = units.Distance(m=1234.0)
+    distance = Distance(m=1234.0)
     assert abs(distance.km - 1.234) < 1e-15
 
 @needs_astropy
 def test_converting_distance_with_astropy():
-    distance = units.Distance(au=1.234)
+    distance = Distance(au=1.234)
     value1 = distance.km
     value2 = distance.to(u.km)
     epsilon = 0.02         # definitions of AU seem to disagree slightly
@@ -62,7 +109,7 @@ def test_converting_distance_with_astropy():
 
 @needs_astropy
 def test_converting_velocity_with_astropy():
-    velocity = units.Velocity(au_per_d=1.234)
+    velocity = Velocity(au_per_d=1.234)
     value1 = velocity.km_per_s
     value2 = velocity.to(u.km / u.s)
     epsilon = 1e-6
