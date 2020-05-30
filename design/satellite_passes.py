943154c4e26994a3eb85e0ebcabe92194a781bc9
Brandon Rhodes <brandon@rhodesmill.org>
2020-05-29 21:31:13 -0400

    Switch script to cheap nutation; change conclusion

--- design/satellite_passes.py
+++ design/satellite_passes.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skyfield import almanac, api
+from skyfield.nutationlib import iau2000b
 
 ts = api.load.timescale(builtin=True)
 eph = api.load('de421.bsp')
@@ -29,6 +30,8 @@ assert list(y[offset:offset+3]) == [0, 1, 2]
 pass_start, pass_end = t[offset+0], t[offset+2]
 
 def f(t):
+    length = t.tt.shape[0]
+    t._nutation_angles = iau2000b(t.tt)
     return sat.at(t).is_sunlit(eph)
 
 f.rough_period = 0.001
@@ -45,19 +48,25 @@ tt = np.arange(pass_start.tt, pass_end.tt, one_second)
 
 T0 = time()
 t = ts.tt_jd(tt)
-is_sunlit = sat.at(t).is_sunlit(eph)
+t._nutation_angles = iau2000b(t.tt)
+satpos = (sat - topos).at(t)
+is_sunlit = satpos.is_sunlit(eph)
 DT = time() - T0
 
 print(DT, 'to compute is_sunlit() for every second of {}-second pass'
       .format(len(tt)))
 
-print('''
-I was hoping the result of the above test would justify abandoning fancy
-event-search routines for short satellite passes, in favor of telling
-users to just go ahead and compute quantities of interest for every
-second over the whole pass.
+T0 = time()
+alt, az, distance = satpos.altaz()
+DT = time() - T0
 
-But the numbers seem to point the other direction: something like a 2x
-speedup is achieved if we search for the moment of change in the sunlit
-status versus just compute it every second.
+print(DT, 'to re-use the positions to compute altitude and azimuth')
+
+print('''
+On my laptop, this script shows that simply computing the positions and
+the sunlit-ness of the ISS for each of the 381 seconds of this pass
+takes roughly the same amount of time as mounting a full search for the
+moment it passes into shadow.  But the benefit is far greater, because
+with almost no additional expense all altitudes and azimuths can also
+be computed.
 ''')
