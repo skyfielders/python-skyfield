1057575a903824ad2cc6529a9ca5544c0bcb1308
Brandon Rhodes <brandon@rhodesmill.org>
2020-07-31 11:45:53 -0400

    Remove script that I just re-wrote by accident

--- update-deltat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-cd "$(dirname ${BASH_SOURCE[0]})"
-cd skyfield/data
-rm -f Leap_Second.dat deltat.data deltat.preds
-python -c 'from skyfield.api import load; load.timescale()' || exit $?
-cp Leap_Second.dat deltat.data deltat.preds ../../ci
