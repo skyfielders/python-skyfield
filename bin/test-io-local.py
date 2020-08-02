28c1011f1032345b8f51678d3ef60b647d981918
Brandon Rhodes <brandon@rhodesmill.org>
2020-08-01 23:21:31 -0400

    Make test download file several ways

--- bin/test-io-local.py
+++ bin/test-io-local.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import tempfile
 import threading
 from contextlib import contextmanager
 
@@ -10,7 +11,7 @@ except ImportError:
     from BaseHTTPServer import HTTPServer
     from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-from skyfield.api import load
+from skyfield.api import Loader
 
 @contextmanager
 def cd(directory):
@@ -29,10 +30,29 @@ def local_web_server():
     yield httpd.server_address
 
 def main():
+    directory = tempfile.mkdtemp()
+    load = Loader(directory)
     here = os.path.dirname(__file__)
     with cd(os.path.join(here, '..', 'ci')):
         with local_web_server() as (host, port):
+            # The first time, it should create the file in the empty directory.
             load('http://{0}:{1}/de421.bsp'.format(host, port), reload=True)
+            filenames = os.listdir(directory)
+            print(filenames)
+            assert filenames == ['de421.bsp']
+
+            # The second time, it should overwrite it.
+            load('http://{0}:{1}/de421.bsp'.format(host, port), reload=True)
+            filenames = os.listdir(directory)
+            print(filenames)
+            assert filenames == ['de421.bsp']
+
+            # The third time, it should make a backup.
+            load('http://{0}:{1}/de421.bsp'.format(host, port), reload=True,
+                 backup=True)
+            filenames = os.listdir(directory)
+            print(filenames)
+            assert filenames == ['de421.bsp', 'de421.old1.bsp']
 
 if __name__ == '__main__':
     main()
