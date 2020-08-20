6b32f5ed7b42912a0ca796acb3f21ac9a4a270f9
glangford <glenn.langford@gmail.com>
2020-08-20 13:59:23 -0400

    Support earth location and update tests

--- design/sgp4_parallel.py
+++ design/sgp4_parallel.py
@@ -7,18 +7,20 @@ conversions from TEME?
 """
 import numpy as np
 from sgp4.api import SatrecArray, SGP4_ERRORS, accelerated
-from skyfield.api import load, utc, EarthSatellite
+from skyfield.api import load, utc, EarthSatellite, Topos
 from skyfield.constants import AU_KM, DAY_S, AU_M
 from skyfield.sgp4lib import TEME_to_ITRF
 from skyfield.positionlib import ITRF_to_GCRS2, build_position
 from skyfield.vectorlib import ObserverData
 
-# SGP4 in parallel across n satellites x m times
-def positions_for(satellites, times):
+# Run SGP4 in parallel across n satellites x m times
+def positions_for(satellites, earthLocation, times):
     sat_array = SatrecArray( [s.model for s in satellites] )
     jd = times._utc_float()
     e, r, v = sat_array.sgp4(jd, np.zeros_like(jd))
     assert len(satellites) == np.shape(r)[0]
+    _, loc_v_GCRS, loc_p_GCRS, _ = earthLocation._at(times)
+    # Unpack the TEME coordinates, convert to GCRS and subtract earthLocation
     for index in range(r.shape[0]):
         errors = e[index]
         messages = [SGP4_ERRORS[error] if error else None for error in errors ]
@@ -26,12 +28,15 @@ def positions_for(satellites, times):
         rTEME = np.divide(r[index].T, AU_KM)
         vTEME = np.divide(v[index].T, AU_KM / DAY_S)
         rITRF, vITRF = TEME_to_ITRF(times.ut1, rTEME, vTEME)
-        rGCRS, vGCRS = ITRF_to_GCRS2(times, rITRF, vITRF)
-        # Adapted from VectorFunction at()
+        sat_p_GCRS, sat_v_GCRS = ITRF_to_GCRS2(times, rITRF, vITRF)
+        # Mirror VectorSum _at() for (EarthSatellite - Topos)
+        sat_p_GCRS -= loc_p_GCRS
+        sat_v_GCRS -= loc_v_GCRS
+        # Mirror VectorFunction at()
         observer_data = ObserverData()
-        observer_data.gcrs_position = rGCRS
+        observer_data.gcrs_position = sat_p_GCRS
         target = -100000 - satellites[index].model.satnum
-        position = build_position(rGCRS, vGCRS, times, 399, target, observer_data)
+        position = build_position(sat_p_GCRS, sat_v_GCRS, times, 399, target, observer_data)
         position.message = messages
         yield position
 
@@ -57,22 +62,19 @@ def test_iss():
     ts = load.timescale(builtin=True)
     L1, L2 = ISS_TLE.splitlines()
     iss = EarthSatellite(L1, L2)
-
     trange = range(0, 60, 10)
     times = ts.utc(2020, 7, 5, 12, 0, trange)
     print("positions_for():")
-    satellites = [iss, iss]
-    for geo1 in positions_for(satellites, times):
+    satellites = [iss]
+    city = Topos('44.0247 N', '88.5426 W')
+    for geo1 in positions_for(satellites, city, times):
         print(geo1.position.km)
-        #print(geo1.message)
     #
     print("Compare to EarthSatellite at():")
-    geo2 = iss.at(times)
+    difference = iss - city
+    geo2 = difference.at(times)
+    #geo2 = iss.at(times)
     print(geo2.position.km)
-    #
-    # Tiny velocity difference compared to `at()` triggered by moving `AU_KM / DAY_S` into np.divide()
-    #np.set_printoptions(precision=16)
-    #print(geo1.velocity.km_per_s - geo2.velocity.km_per_s)
     #
     assert np.isclose(geo1.position.km, geo2.position.km).all()
     one_m_per_hour = 1.0 * 24.0 / AU_M
@@ -86,26 +88,27 @@ def test_two_sats():
     L1, L2 = KESTREL_TLE.splitlines()
     kestrel = EarthSatellite(L1, L2)
     #
-    trange = range(0, 60, 10)
+    city = Topos('44.0247 N', '88.5426 W')
+    trange = range(0, 300, 10)
     times = ts.utc(2020, 7, 5, 12, 0, trange)
     satellites = [kestrel, iss]
-    positions = list(positions_for(satellites, times))
-    kestrel1, iss1  = positions
-    iss2 = iss.at(times)
-    kestrel2 = kestrel.at(times)
+    positions = list(positions_for(satellites, city, times))
+    kestrel1, iss1 = positions
+    iss2 = (iss - city).at(times)
+    kestrel2 = (kestrel - city).at(times)
     assert np.isclose(iss1.position.km, iss2.position.km).all()
     assert np.isclose(kestrel1.position.km, kestrel2.position.km).all()
 
-# With optimized SGP4, peak memory use 142MB and runs in ~1s on my laptop
 def test_many_sats():
     print("test many sats")
     ts = load.timescale(builtin=True)
     active_sats = load.tle_file("https://celestrak.com/NORAD/elements/active.txt", reload=False)
-    print("  #active satellites:", len(active_sats), " x 300 time values")
+    print(" active satellites:", len(active_sats), " x 300 time values")
     trange = range(0, 300, 1)
     times = ts.utc(2020, 7, 5, 12, 0, trange)
-    locations = list(positions_for(active_sats, times))
-    assert len(active_sats) == len(locations)
+    city = Topos('44.0247 N', '88.5426 W')
+    positions = list(positions_for(active_sats, city, times))
+    assert len(active_sats) == len(positions)
 
 if __name__ == "__main__":
     main()
