36e3b7b093a0faee61bc48673162a43cb3243b87
Brandon Rhodes <brandon@rhodesmill.org>
2020-10-24 10:39:59 -0400

    Bump version to 1.31

--- skyfield/__init__.py
+++ skyfield/__init__.py
@@ -5,5 +5,5 @@ functions and classes there.  See the ``documentation`` directory inside
 the source code, as well as the http://rhodesmill.org/skyfield/ site!
 
 """
-VERSION = (1, 30)
+VERSION = (1, 31)
 __version__ = '.'.join(map(str, VERSION))
