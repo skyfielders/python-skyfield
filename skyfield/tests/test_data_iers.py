f384a5d77cae62b6487d039e4e8dd51ef9cdc258
Brandon Rhodes <brandon@rhodesmill.org>
2021-03-17 17:03:45 -0400

    Add explicit test for finals2000A.all converter

---
+++ skyfield/tests/test_data_iers.py
@@ -0,0 +1,16 @@
+from numpy import array, inf
+from skyfield.data.iers import _build_timescale_arrays
+
+def test_build_timescale_arrays():
+    mjd = array([42046.00, 42047.00, 42048.00, 42049.00])
+    dut1 = array([-0.2942581, -0.2971424, 0.6999438, 0.6970539])
+    delta_t, leap_dates, leap_offsets = _build_timescale_arrays(mjd, dut1)
+    column_1, column_2 = delta_t
+
+    assert list(column_1) == [2442046.5005113888, 2442047.5005113888,
+                              2442048.500522963, 2442049.500522963]
+    assert list(column_2) == [44.4782581, 44.481142399999996,
+                              44.4840562, 44.4869461]
+    assert list(leap_dates) == [-inf, 2441317.5, 2441499.5, 2441683.5,
+                                2442048.5, inf]
+    assert list(leap_offsets) == [10, 10, 10, 11, 12, 13]
