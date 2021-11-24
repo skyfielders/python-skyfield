ec3682fac10e083032fe32bdfbec2b324597ce0c
Josh Paterson <mail@joshpaterson.com>
2021-11-24 03:15:27 -0500

    Make comparison more explicit

--- skyfield/elementslib.py
+++ skyfield/elementslib.py
@@ -24,7 +24,7 @@ def osculating_elements_of(position, reference_frame=None, gm_km3_s2=None):
     :class:`~skyfield.elementslib.OsculatingElements`
 
     """
-    if not gm_km3_s2:
+    if gm_km3_s2 is not None:
         gm_km3_s2 = GM_dict.get(position.center, 0.0)
         if not 0 <= position.center <= 9: # true if position.center is not a barycenter
             gm_km3_s2 += GM_dict.get(position.target, 0.0)
