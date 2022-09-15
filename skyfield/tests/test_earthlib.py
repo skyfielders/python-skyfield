b927868869c5fdf4574d80229bcb231c5bc6be34
Brandon Rhodes <brandon@rhodesmill.org>
2022-09-15 09:14:01 -0400

    Only test for 1 mm precision on 1 AU vector

--- skyfield/tests/test_earthlib.py
+++ skyfield/tests/test_earthlib.py
@@ -4,7 +4,8 @@ from skyfield.earthlib import AU_M, ERAD, reverse_terra, tau
 def test_reverse_terra_with_zero_iterations():
     # With zero iterations, should return "geocentric" rather than
     # "geodetic" (="correct") longitude and latitude.
-    lat, lon, elevation = reverse_terra(array([1, 0, 1]), 0, iterations=0)
+    lat, lon, elevation_m = reverse_terra(array([1, 0, 1]), 0, iterations=0)
+    one_millimeter = 1e-3
     assert abs(lat - tau / 8) < 1e-16
     assert lon == 0.0
-    assert abs(elevation - (AU_M * sqrt(2) - ERAD)) < 1e-16
+    assert abs(elevation_m - (AU_M * sqrt(2) - ERAD)) < one_millimeter
