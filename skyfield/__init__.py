33478b7195a819e1b4f07f78ba1b8ae1b3c83e38
Brandon Rhodes <brandon@rhodesmill.org>
2022-09-15 09:03:08 -0400

    Actually bump the version to 1.45

--- skyfield/__init__.py
+++ skyfield/__init__.py
@@ -5,5 +5,5 @@ functions and classes there.  See the ``documentation`` directory inside
 the source code, as well as the http://rhodesmill.org/skyfield/ site!
 
 """
-VERSION = 1, 44
+VERSION = 1, 45
 __version__ = '.'.join(map(str, VERSION))
