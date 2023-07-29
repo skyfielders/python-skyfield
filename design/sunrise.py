459470654eb3e0127e84b69077049940d0f0f357
Brandon Rhodes <brandon@rhodesmill.org>
2023-07-28 22:16:16 -0400

    In new routine, detect when object doesn't rise

--- design/sunrise.py
+++ design/sunrise.py
@@ -11,13 +11,22 @@ from skyfield.api import load, tau
 
 #cos HA = (sin ALT - sin LAT sin DEC) / (cos LAT cos DEC)
 
-from numpy import arccos, sin, cos
+from numpy import arccos, sin, clip, cos
 
 def _sunrise_hour_angle_radians(latitude, declination, altitude_radians):
+    """Return the hour angle at which a body will reach a given altitude.
+
+    Given the latitude of an observer, and the declination of a target,
+    return the positive hour angle at which the body will set below the
+    horizon, where the horizon is specified as `altitude_radians` above
+    (positive) or below (negative) the great circle of zero altitude.
+
+    """
     lat = latitude.radians
     dec = declination.radians
-    ha = arccos((sin(altitude_radians) - sin(lat) * sin(dec))
-                / (cos(lat) * cos(dec)))
+    numerator = sin(altitude_radians) - sin(lat) * sin(dec)
+    denominator = cos(lat) * cos(dec)
+    ha = arccos(clip(numerator / denominator, -1.0, 1.0))
     return ha
 
 import numpy as np
@@ -55,35 +64,26 @@ def find_sunrise(observer, target, horizon_degrees, start_time, end_time):
     # throw the first few out and keep only the last one.
     i, = np.nonzero(np.diff(difference) < 0.0)
 
-    # When might have rising actually have taken place?  Let's guess by
-    # drawing a line between the two (time, hour angles) coordinates and
-    # finding where they intersect zero.
+    # When might each rising have actually taken place?  Let's
+    # interpolate between the two times that bracket each rising.
     a = tau - difference[i]
     b = difference[i + 1]
     tt = t.tt
-    new_tt = (b * tt[i] + a * tt[i+1]) / (a + b)
-    t = ts.tt_jd(new_tt)
+    interpolated_tt = (b * tt[i] + a * tt[i+1]) / (a + b)
+    t = ts.tt_jd(interpolated_tt)
 
     ha_per_day = tau
 
-    for i in 0, 1: #2, 3:
+    for i in 0, 1:
         fastify(t)
         ha, dec, _ = observer.at(t).observe(target).apparent().hadec()
         desired_ha = - _sunrise_hour_angle_radians(latitude, dec, h)
         ha_adjustment = desired_ha - ha.radians
-        print('ha_adjustment:', ha_adjustment)
         timebump = ha_adjustment / ha_per_day
-        print('Timebump(s):', timebump * 24.0 * 3600.0)
         t = ts.tt_jd(t.whole, t.tt_fraction + timebump)
 
-    # Test of how we did.
-    t_final = t
-    alt, az, _ = observer.at(t_final).observe(target).apparent().altaz()
-    diff = horizon_degrees - alt.degrees
-    print('New alts:', diff.min(), 'to', diff.max(), 'degrees')
-    print('Detailed differences (degrees):', diff)
-
-    return t
+    is_above_horizon = (desired_ha != 0.0)
+    return t, is_above_horizon
 
 #find_sunrise(earth + bluffton, sun, stdalt, t0, t1)
 
@@ -91,12 +91,12 @@ def main():
     ts = load.timescale()
     eph = load('de421.bsp')
     earth = eph['earth']
-    #bluffton = earth + api.wgs84.latlon(40.8939, -83.8917)
     bluffton = api.wgs84.latlon(40.8939, -83.8917)
+    #bluffton = api.wgs84.latlon(80.8939, -83.8917)
 
     t0 = ts.utc(2020, 1, 1)
-    t1 = ts.utc(2020, 1, 10)
-    #t1 = ts.utc(2021, 1, 1)
+    #t1 = ts.utc(2020, 1, 10)
+    t1 = ts.utc(2021, 1, 1)
     # t0 = ts.utc(2018, 9, 12, 4)
     # t1 = ts.utc(2018, 9, 13, 4)
 
@@ -118,10 +118,9 @@ def main():
         T0 = time()
         f = almanac.risings_and_settings(
             eph, sun, bluffton,
-            horizon_degrees=-stdalt,
+            horizon_degrees=stdalt,
             radius_degrees=0.0,
         )
-        #f = almanac.sunrise_sunset(eph, bluffton)
         t_old, y = almanac.find_discrete(t0, t1, f)
         DUR_OLD = time() - T0
 
@@ -135,7 +134,10 @@ def main():
     # print(y)
 
     T0 = time()
-    t_new = find_sunrise(earth + bluffton, sun, stdalt, t0, t1)
+    t_new, is_above_horizon = find_sunrise(
+        earth + bluffton, sun, stdalt, t0, t1,
+    )
+    print('sum is_above_horizon:', is_above_horizon.sum())
     DUR_NEW = time() - T0
 
     t_old = t_old[::2]
