cdf4407e4c5c55c9d17ca7f296367fdf71c4f3d0
Brandon Rhodes <brandon@rhodesmill.org>
2024-01-13 09:52:40 -0500

    Declare version 1.47

--- skyfield/__init__.py
+++ skyfield/__init__.py
@@ -5,5 +5,5 @@ functions and classes there.  See the ``documentation`` directory inside
 the source code, as well as the http://rhodesmill.org/skyfield/ site!
 
 """
-VERSION = 1, 46
+VERSION = 1, 47
 __version__ = '.'.join(map(str, VERSION))
