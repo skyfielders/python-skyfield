bed9001428e965a29173beb97f888cb7d2900822
Brandon Rhodes <brandon@rhodesmill.org>
2024-09-01 12:23:34 -0400

    More work on diagrams showing search for moonrise

--- design/illustrate_moonrise_edge_case.py
+++ design/illustrate_moonrise_edge_case.py
@@ -47,7 +47,7 @@ for star in stars:
     alt, az, distance = observer.at(t).observe(star).apparent().altaz()
     star_x = az.degrees
     star_y = alt.degrees
-    star_curves.append([star_x, star_y])
+    star_curves.append([star, star_x, star_y])
 
 alt, az, distance = observer.at(t).observe(moon).apparent().altaz()
 moon_x = az.degrees
@@ -56,36 +56,68 @@ moon_y = alt.degrees
 horizon = -0.8444219217855957  # later, compute this instead of hard-coding
 arrow_style = {'arrowstyle': 'simple', 'lw': 0.2, 'fc': 'black'}
 
-def make_plot(target):
+def make_plot(target, horizon_degrees=None):
     trace.clear()
-    t, y = almanac.find_risings(observer, target, t0, t1)
-    traced_times = trace[1:]
+    t, y = almanac.find_risings(observer, target, t0, t1, horizon_degrees)
+    traced_times = trace[:]
 
     fig, ax = plt.subplots()
 
-    for star_x, star_y in star_curves:
+    for star, star_x, star_y in star_curves:
         ax.plot(star_x, star_y, 'b')
+        ax.annotate(f'{star.dec.degrees}°', (star_x[-1], star_y[-1]), (-23, -9),
+                    'data', 'offset points')
 
-    ax.plot(moon_x, moon_y, '.')
+    if target is moon:
+        ax.plot(moon_x, moon_y, '.')
+
     ax.grid()
 
     degree = api.tau / 360.0
-    arrow_angle = 90.0 * degree
+    arrow_angle = 80.0 * degree
     arrow_length = 25.0
     for i, t in enumerate(traced_times):
+        print(t.utc_strftime())
+        if i == 0:
+            continue
         alt, az, distance = observer.at(t).observe(target).apparent().altaz()
         ax.plot(az.degrees, alt.degrees, 'o')
         arrow_angle -= 20.0 * degree
         arrow_x = np.cos(arrow_angle) * arrow_length
         arrow_y = np.sin(arrow_angle) * arrow_length
-        ax.annotate(str(i + 1), (az.degrees, alt.degrees), (arrow_x, arrow_y),
+        ax.annotate(f'{i}  ({t[0].utc_strftime("%H:%M:%S")})',
+                    (az.degrees, alt.degrees), (arrow_x, arrow_y),
                     'data', 'offset points', arrow_style)
 
-    ax.set(xlabel='Azimuth (°)', ylabel='Altitude (°)', title='Moonrise')
+    if 0: #target is moon:
+        # Trying to find the intersection by hand:
+        ty = traced_times[-2][0]
+        tz = traced_times[-1][0]
+        alty, az, distance = observer.at(ty).observe(target).apparent().altaz()
+        altz, az, distance = observer.at(tz).observe(target).apparent().altaz()
+
+        ay = alty.radians - -0.0147393
+        az = altz.radians - -0.0147393
+        t = ty - ay / ((az - ay) / (tz - ty))
+        # t = ty - ay / (az - ay) * (tz - ty)
+        print(ty.utc_strftime(), ay)
+        print(tz.utc_strftime(), az)
+        print(t.utc_strftime())
+
+        alt, az, distance = observer.at(t).observe(target).apparent().altaz()
+        print(alt.degrees, az.degrees)
+
+        ax.plot(az.degrees, alt.degrees, 'o')
+        arrow_angle -= 20.0 * degree
+        ax.annotate(str('end'), (az.degrees, alt.degrees), (arrow_x, arrow_y),
+                    'data', 'offset points', arrow_style)
+
+    ax.set(xlabel='Azimuth (°)', ylabel='Altitude (°)',
+           title='The search for the moment of rising on 2023-02-19')
     ax.axhline(horizon)
     ax.axvline(180.0)
     return fig
 
-#fig = make_plot(stars[1])
-fig = make_plot(stars[3])
-fig.savefig('tmp.png')
+make_plot(stars[2], horizon).savefig('tmp1.png')
+make_plot(stars[1], horizon).savefig('tmp2.png')
+make_plot(moon).savefig('tmp3.png')
