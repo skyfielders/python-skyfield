bfca90b1828d8b2249dd66bc8b8c87a9df8fbbbc
Brandon Rhodes <brandon@rhodesmill.org>
2025-02-23 11:56:40 -0500

    Drop Python 2.6 classifier; we don’t test for 2.6

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
@@ -27,20 +26,20 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Astronomy',
-        ],
+    ],
     packages=[
         'skyfield',
         'skyfield.data',
         'skyfield.tests',
-        ],
+    ],
     package_data = {
         'skyfield.data': ['*.gz', '*.npy', '*.npz'],
         'skyfield.tests': ['data/*'],
-        },
+    },
     install_requires=[
         'certifi>=2017.4.17',  # version constraint copied from Requests
         'jplephem>=2.13',
         'numpy',
         'sgp4>=2.2',
-        ],
+    ],
 )
