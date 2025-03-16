18d0ba0520dae5e217b9fc8b0c4df48e63e02161
Brandon Rhodes <brandon@rhodesmill.org>
2025-03-16 08:56:32 -0400

    Add test of Earth deflection effect

--- skyfield/tests/test_earth_deflection.py
+++ skyfield/tests/test_earth_deflection.py
@@ -1,31 +1,55 @@
-"""Hand-crafted tests against specific NOVAS behaviors.
+"""Test that Skyfield implements NOVAS-compatible Earth deflection.
 
-The tests in the neighboring `tests_against_novas.py` are automatically
-generated and in general demonstrate close agreement with NOVAS.  But
-the hand-crafted tests here are aimed at specific edge conditions that
-we want to make sure we get correct.
+Run `design/measure_earth_deflection.py` to draw a quick graph.
 
 """
 from numpy import arange, diff
-from skyfield.api import Topos, load
+from skyfield import relativity
+from skyfield.api import load, wgs84
 
-def test_earth_deflection():
+def test_earth_deflection_magnitude_and_direction():
+    ts = load.timescale()
+    t = ts.tt(2015, 3, 16, arange(0, 25, 2))
+    eph = load('de421.bsp')
+    earth = eph['earth']
+    jupiter = eph['jupiter barycenter']
+    lowell = earth + wgs84.latlon(35.2029, -111.6646)
+    alt1, az1, _ = lowell.at(t).observe(jupiter).apparent().altaz()
+
+    old = relativity.rmasses['earth']
+    relativity.rmasses['earth'] = 1e100  # almost no mass
+    alt2, az2, _ = lowell.at(t).observe(jupiter).apparent().altaz()
+    relativity.rmasses['earth'] = old
+
+    dalt = ' '.join('%.2f' % n for n in alt1.mas() - alt2.mas())
+    daz = ' '.join('%.2f' % n for n in az1.mas() - az2.mas())
+
+    # These values have not been checked against an external authority.
+    # They are simply what Skyfield 1.51 did when tested.  But at least
+    # this test prevents us from changing behavior in the future without
+    # knowing it.  It's at least encouraging that Earth deflection
+    # affects altitude not azimuth, and bumps altitude up.
+
+    s = '0.19 0.11 0.06 0.06 0.12 0.19 0.30 0.00 0.00 0.00 0.00 0.29 0.19'
+    assert dalt == s
+
+    s = '0.00 0.00 0.00 -0.00 -0.00 -0.00 -0.00 0.00 0.00 0.00 0.00 0.00 0.00'
+    assert daz == s
+
+def test_earth_deflection_cutoff():
     # The NOVAS library includes the Earth's gravitational deflection of
     # light for both topocentric observers and observers in Earth orbit,
     # but shuts this effect off once the object is behind the Earth 20%
     # of the way from its limb towards its center.  This test determines
     # whether Skyfield puts the resulting discontinuity in the same
     # place as the NOVAS library does.
-    #
-    # For more details see:
-    # https://github.com/skyfielders/astronomy-notebooks/blob/master/Skyfield-Notes/Fixing-earth-deflection.ipynb
 
-    t = load.timescale(delta_t=0.0)
-    t = t.tt(2016, 7, 2, arange(10.5628, 10.5639, 0.0002))
+    ts = load.timescale(delta_t=0.0)
+    t = ts.tt(2016, 7, 2, arange(10.5628, 10.5639, 0.0002))
     planets = load('de405.bsp')
     earth = planets['earth']
     mars = planets['mars']
-    lowell = earth + Topos(latitude_degrees=35.2029, longitude_degrees=-111.6646)
+    lowell = earth + wgs84.latlon(35.2029, -111.6646)
     ra, dec, distance = lowell.at(t).observe(mars).apparent().radec()
     h = ra.hours
     hprime = diff(h)
