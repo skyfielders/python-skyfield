a40cde400ae1a53057f1ad535e2b7349254c3888
Brandon Rhodes <brandon@rhodesmill.org>
2025-04-07 18:53:43 -0400

    Reduce test BSP size, since it ships with Skyfield
    Gads, how did I wind up shipping such a huge test file with Skyfield?
    Irresponsible.  Not sure what I was thinking.  Apologies, everyone.

--- skyfield/tests/test_jpllib.py
+++ skyfield/tests/test_jpllib.py
@@ -4,9 +4,12 @@ from skyfield.api import load, load_file
 def _data_path(filename):
     return os.path.join(os.path.dirname(__file__), 'data', filename)
 
+# Test file generated with:
+# python -m jplephem excerpt 1969/07/29 1969/07/30 de441.bsp de441-1969.bsp
+
 def test_multiple_non_overlapping_segments_per_target():
     ts = load.timescale()
-    t = ts.utc(1969, [1, 4, 8, 12])
+    t = ts.utc(1969, 7, [28, 29, 30, 31])
     eph = load_file(_data_path('./de441-1969.bsp'))
 
     pluto = eph['pluto barycenter']
@@ -14,9 +17,9 @@ def test_multiple_non_overlapping_segments_per_target():
     assert len(pluto.segments) == 2
 
     assert str(pluto.at(t).xyz) == (
-        '[[-30.53621462 -30.51057842 -30.47219562 -30.42961491]\n'
-        ' [ -0.31275432  -0.59574835  -0.97928763  -1.36269213]\n'
-        ' [  9.10213562   9.00611101   8.87487322   8.74241232]] au'
+        '[[-30.47352052 -30.47318972 -30.47285863 -30.47252727]\n'
+        ' [ -0.96671442  -0.96985773  -0.97300104  -0.97614434]\n'
+        ' [  8.87919555   8.87811509   8.87703455   8.87595392]] au'
     )
 
     # Does the Stack correctly offer its .ephemeris to apparent()?
